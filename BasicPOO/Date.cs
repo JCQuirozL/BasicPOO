7a24db3d1fe9cb0c2f142ac8d12e3c1b8b873204
compuexpressmanzanillo <compuexpressmanzanillo@outlook.com>
2022-02-21 14:30:48 -0600

    Agregar archivos de proyecto.

---
+++ BasicPOO/Date.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPOO
+{
+    internal class Date
+    {
+        public int _year;
+        public int _month;
+        public int _day;
+
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = IsValidMonth(month); 
+            _day = IsValidDay(day);
+        }
+
+        private int IsValidDay(int day)
+        {
+            if (day <= 31)
+            {
+                if (_month == 2 && day == 29 && !IsLeapYear(_year))
+                {
+                    throw new DayException("Sorry, invalid day.");
+                }
+
+            }
+
+            return day;
+           
+           
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+            
+        }
+
+        private int IsValidMonth(int month)
+        {
+            if (month == 0 || month > 12)
+            {
+                throw new MonthException("Sorry, Invalid Month.");
+            }
+            return month;
+        }
+
+        public override string ToString()
+        {
+            return $"{_year}/{_month}/{_day}";
+        }
+    }
+    
+}
+
+
+
---
+++ BasicPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using BasicPOO;
+
+Console.WriteLine("**************");
+Console.WriteLine("* POO Basics *");
+Console.WriteLine("**************");
+
+try
+{
+    Console.WriteLine(new Date(2022,2,29).ToString());
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
+
+
